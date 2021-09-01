6d221033ae33202bca9071e246b0eb0ddf4d4fec
Nicholas Rawitscher <nicholas@bimorph.com>
2021-09-01 09:20:39 -0500

    Added IGeometryConverter

---
+++ HbConnector.Core/Interfaces/IGeometryConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HB.RestAPI.Core.Interfaces;
+
+namespace HbConnector.Core.Interfaces
+{
+    
+    public interface IGeometryConverter<in T>
+    {
+        IHbObject ToHbType(T mesh);
+    }
+}
